d264d99138da4a1e173356e311620a1bc917d2dc
Parash Giri <parashgiri@Parashs-MacBook-Pro.local>
2024-03-21 22:30:58 +0000

    added changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const cors = require("cors");
-import serverless from "serverless-http";
+
 
 const app = express();
 
@@ -54,54 +54,8 @@ app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
 
-// const port = process.env.port || 8000;
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-
-export const handler = serverless(app);
-
-
-// const express = require("express");
-// const cors = require("cors");
-// const serverless = require("serverless-http"); // Change import statement
-
-// const app = express();
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cors({ origin: '*' }));
-
-// Routers
-// const authRoutes = require("./src/Router/auth_routes");
-// const categoryroutes = require("./src/Router/category_routes");
-// const sizeroutes = require("./src/Router/size_routes");
-// const colorroutes = require("./src/Router/color_routes");
-// const brandroutes = require("./src/Router/brand_routes");
-// const productroutes = require("./src/Router/product_routes");
-// const cartroutes = require("./src/Router/cart_routes");
-// const addressroutes = require("./src/Router/address_routes");
-// const orderroutes = require("./src/Router/order_routes");
-// const contactroutes = require("./src/Router/contact_routes");
-// const carouselroutes = require("./src/Router/carousel_routes");
-
-// app.use("/", express.static("Images")); // Serve static images
-// app.use(authRoutes);
-// app.use(categoryroutes);
-// app.use(sizeroutes);
-// app.use(colorroutes);
-// app.use(brandroutes);
-// app.use(productroutes);
-// app.use(cartroutes);
-// app.use(addressroutes);
-// app.use(orderroutes);
-// app.use(contactroutes);
-// app.use(carouselroutes);
-
-// // Sample route to get started
-// app.get("/", (req, res) => {
-//   res.send("Hello, Express!");
-// });
-
-// // Export app for Netlify handler
-// module.exports = app;
+const port = process.env.port || 8000;
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
